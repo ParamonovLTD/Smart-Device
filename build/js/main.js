6f46b31334a2602d72c533b9430814231e1d64e3
SnowballBoy <54330340+SnowballBoy@users.noreply.github.com>
2020-08-21 21:15:00 +0300

    Добавляет хедер

---
+++ build/js/main.js
@@ -0,0 +1,182 @@
+'use strict';
+
+(function () {
+const body = document.body;
+const header = document.querySelector('.header');
+const headerLinks = document.querySelectorAll('.header__contacts-wrapper > a');
+console.log(headerLinks);
+function onJSLoad () {
+}
+onJSLoad();
+
+function onTabletWidth () {
+  if (window.matchMedia("(max-width: 1023px)").matches) {
+    headerLinks.forEach((link) => {
+      link.textContent = "";
+    })
+  }
+}
+onTabletWidth();
+
+// function onDOMLoaded () {
+//   const telInputs = document.querySelectorAll('input[type="tel"]');
+//   const mailInputs = document.querySelectorAll('input[type="email"]');
+
+//   telInputs.forEach((telInput) => {
+//     telInput.value = JSON.parse(localStorage.getItem('phoneNumber'));
+//   })
+//   mailInputs.forEach((mailInput) => {
+//     mailInput.value = JSON.parse(localStorage.getItem('emailAddress'));
+//   })
+// }
+
+
+// function DOMLoaded () {
+//   document.addEventListener('DOMContentLoaded', onDOMLoaded);
+// }
+// DOMLoaded();
+
+
+// function menuToggle () {
+//   headerNav.classList.toggle('nav--opened');
+//   headerBurger.classList.toggle('header__burger--opened');
+//   headerLogo.classList.toggle('header__logo-img--light');
+// }
+
+// headerBurger.addEventListener('click', menuToggle);
+
+
+// function onMenuFullWidth () {
+//   if (document.documentElement.clientWidth === headerNav.offsetWidth) {
+//     body.classList.add('body--scroll-locked');
+//   } else {
+//     body.classList.remove('body--scroll-locked');
+//   }
+// }
+
+// function menuFullWidth () {
+//   headerBurger.addEventListener('click', onMenuFullWidth);
+// }
+// menuFullWidth();
+
+
+function smoothAnimationForAnchors () {
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  anchors.forEach((anchor) => {
+    anchor.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      const blockId = anchor.getAttribute('href');
+
+      document.querySelector('' + blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+}
+smoothAnimationForAnchors();
+
+
+// function numberMaskSet() {
+//   const telInput = document.querySelectorAll('.form input[type="tel"]');
+
+//   telInput.forEach(function (it) {
+//     Inputmask("+7 999 999 99 99", {
+//       placeholder: '',
+//       showMaskOnHover: false,
+//       showMaskOnFocus: false
+//     }).mask(it);
+//   });
+// }
+// numberMaskSet();
+
+
+// const feedback = document.querySelector('.feedback');
+
+
+// function formValidityCheck (currentFormWrapper) {
+//   const currentForm = currentFormWrapper.querySelector('form');
+//   currentForm.setAttribute('novalidate', 'novalidate');
+
+//   currentForm.addEventListener('submit', (evt) => {
+//     evt.preventDefault();
+
+//     if (formInputsValidityCheck(currentForm)) {
+//       const currentTelInputValue = currentForm.querySelector('input[type="tel"]').value;
+//       const currentMailInputValue = currentForm.querySelector('input[type="email"]').value;
+
+//       currentForm.reset();
+//       localStorage.setItem('phoneNumber', JSON.stringify(currentTelInputValue));
+//       localStorage.setItem('emailAddress', JSON.stringify(currentMailInputValue));
+//     }
+//   });
+// }
+// formValidityCheck(feedback);
+
+// function formInputsValidityCheck (currentForm) {
+//   const name = currentForm.querySelector('input[name="Name"]');
+//   const phone = currentForm.querySelector('input[type="tel"]');
+//   const mail = currentForm.querySelector('input[type="email"]');
+//   const check = currentForm.querySelector('input[type="checkbox"]');
+//   const nameValue = name.value.trim();
+//   const phoneValue = phone.value.trim();
+//   const mailValue = mail.value.trim();
+//   let phoneValueSplited;
+//   let isNameValid = false;
+//   let isPhoneValid = false;
+//   let isMailValid = true;
+//   let isChecked = false;
+
+//   if (phoneValue) {
+//     phoneValueSplited = phoneValue.split('+')[1].split(' ').join('').trim();
+//   }
+
+//   if (nameValue !== '') {
+//     isNameValid = true;
+//     setSuccessFor(name);
+//   } else {
+//     setErrorFor(name, 'Данные не верны');
+//   }
+
+//   if (phoneValue && phoneValueSplited.length !== 11) {
+//     setErrorFor(phone, 'Данные не верны');
+//   } else if (phoneValue === '') {
+//     setErrorFor(phone, 'Данные не верны');
+//   } else {
+//     isPhoneValid = true;
+//     setSuccessFor(phone);
+//   }
+
+//   if (!mailValue.includes('@') || mailValue === '') {
+//     isMailValid = false;
+//     setErrorFor(mail, 'Данные не верны');
+//   } else {
+//     setSuccessFor(mail);
+//   }
+
+//   if (check.checked) {
+//     isChecked = true;
+//     setSuccessFor(check);
+//   } else {
+//     setErrorFor(check, 'У вас нет выбора)');
+//   }
+
+//   return isPhoneValid && isMailValid && isNameValid && isChecked;
+// }
+
+
+// function setErrorFor (input, message) {
+//   const inputControl = input.parentElement;
+//   const small = inputControl.querySelector('small');
+//   inputControl.classList.add('form__input-control--error');
+//   small.textContent = message;
+// }
+
+
+// function setSuccessFor (input) {
+//   const inputControl = input.parentElement;
+//   inputControl.classList.remove('form__input-control--error');
+// }
+})();
