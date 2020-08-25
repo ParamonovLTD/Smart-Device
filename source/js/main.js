656bbb61b57125d26b3145f9accd05dbce0896ea
SnowballBoy <54330340+SnowballBoy@users.noreply.github.com>
2020-08-25 16:33:02 +0300

    Добавляет главную форму

--- source/js/main.js
+++ source/js/main.js
@@ -40,23 +40,23 @@ function onMobileWidth () {
 onMobileWidth();
 
 
-// function onDOMLoaded () {
-//   const telInputs = document.querySelectorAll('input[type="tel"]');
-//   const mailInputs = document.querySelectorAll('input[type="email"]');
+function onDOMLoaded () {
+  const telInputs = document.querySelectorAll('input[type="tel"]');
+  const nameInputs = document.querySelectorAll('input[name="Name"]');
 
-//   telInputs.forEach((telInput) => {
-//     telInput.value = JSON.parse(localStorage.getItem('phoneNumber'));
-//   })
-//   mailInputs.forEach((mailInput) => {
-//     mailInput.value = JSON.parse(localStorage.getItem('emailAddress'));
-//   })
-// }
+  telInputs.forEach((telInput) => {
+    telInput.value = JSON.parse(localStorage.getItem('phoneNumber'));
+  })
+  nameInputs.forEach((nameInput) => {
+    nameInput.value = JSON.parse(localStorage.getItem('Name'));
+  })
+}
 
 
-// function DOMLoaded () {
-//   document.addEventListener('DOMContentLoaded', onDOMLoaded);
-// }
-// DOMLoaded();
+function DOMLoaded () {
+  document.addEventListener('DOMContentLoaded', onDOMLoaded);
+}
+DOMLoaded();
 
 
 // function menuToggle () {
@@ -101,104 +101,115 @@ function smoothAnimationForAnchors () {
 smoothAnimationForAnchors();
 
 
-// function numberMaskSet() {
-//   const telInput = document.querySelectorAll('.form input[type="tel"]');
+function numberMaskSet() {
+  const telInput = document.querySelectorAll('.form input[type="tel"]');
 
-//   telInput.forEach(function (it) {
-//     Inputmask("+7 999 999 99 99", {
-//       placeholder: '',
-//       showMaskOnHover: false,
-//       showMaskOnFocus: false
-//     }).mask(it);
-//   });
-// }
-// numberMaskSet();
-
-
-// const feedback = document.querySelector('.feedback');
+  telInput.forEach(function (it) {
+    Inputmask("+7 999 999 99 99", {
+      placeholder: '',
+      showMaskOnHover: false,
+      showMaskOnFocus: false
+    }).mask(it);
+  });
+}
+numberMaskSet();
 
 
-// function formValidityCheck (currentFormWrapper) {
-//   const currentForm = currentFormWrapper.querySelector('form');
-//   currentForm.setAttribute('novalidate', 'novalidate');
-
-//   currentForm.addEventListener('submit', (evt) => {
-//     evt.preventDefault();
-
-//     if (formInputsValidityCheck(currentForm)) {
-//       const currentTelInputValue = currentForm.querySelector('input[type="tel"]').value;
-//       const currentMailInputValue = currentForm.querySelector('input[type="email"]').value;
-
-//       currentForm.reset();
-//       localStorage.setItem('phoneNumber', JSON.stringify(currentTelInputValue));
-//       localStorage.setItem('emailAddress', JSON.stringify(currentMailInputValue));
-//     }
-//   });
-// }
-// formValidityCheck(feedback);
-
-// function formInputsValidityCheck (currentForm) {
-//   const name = currentForm.querySelector('input[name="Name"]');
-//   const phone = currentForm.querySelector('input[type="tel"]');
-//   const mail = currentForm.querySelector('input[type="email"]');
-//   const check = currentForm.querySelector('input[type="checkbox"]');
-//   const nameValue = name.value.trim();
-//   const phoneValue = phone.value.trim();
-//   const mailValue = mail.value.trim();
-//   let phoneValueSplited;
-//   let isNameValid = false;
-//   let isPhoneValid = false;
-//   let isMailValid = true;
-//   let isChecked = false;
-
-//   if (phoneValue) {
-//     phoneValueSplited = phoneValue.split('+')[1].split(' ').join('').trim();
-//   }
-
-//   if (nameValue !== '') {
-//     isNameValid = true;
-//     setSuccessFor(name);
-//   } else {
-//     setErrorFor(name, 'Данные не верны');
-//   }
-
-//   if (phoneValue && phoneValueSplited.length !== 11) {
-//     setErrorFor(phone, 'Данные не верны');
-//   } else if (phoneValue === '') {
-//     setErrorFor(phone, 'Данные не верны');
-//   } else {
-//     isPhoneValid = true;
-//     setSuccessFor(phone);
-//   }
-
-//   if (!mailValue.includes('@') || mailValue === '') {
-//     isMailValid = false;
-//     setErrorFor(mail, 'Данные не верны');
-//   } else {
-//     setSuccessFor(mail);
-//   }
-
-//   if (check.checked) {
-//     isChecked = true;
-//     setSuccessFor(check);
-//   } else {
-//     setErrorFor(check, 'У вас нет выбора)');
-//   }
-
-//   return isPhoneValid && isMailValid && isNameValid && isChecked;
-// }
+const feedback = document.querySelector('.feedback');
 
 
-// function setErrorFor (input, message) {
-//   const inputControl = input.parentElement;
-//   const small = inputControl.querySelector('small');
-//   inputControl.classList.add('form__input-control--error');
-//   small.textContent = message;
-// }
+function formValidityCheck (currentFormWrapper) {
+  const currentForm = currentFormWrapper.querySelector('form');
+  currentForm.setAttribute('novalidate', 'novalidate');
+
+  currentForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    if (formInputsValidityCheck(currentForm)) {
+      const currentTelInputValue = currentForm.querySelector('input[type="tel"]').value;
+      const currentNameInputValue = currentForm.querySelector('input[name="Name"]').value;
+
+      currentForm.reset();
+      localStorage.setItem('phoneNumber', JSON.stringify(currentTelInputValue));
+      localStorage.setItem('Name', JSON.stringify(currentNameInputValue));
+    }
+  });
+}
+formValidityCheck(feedback);
+
+function formInputsValidityCheck (currentForm) {
+  const name = currentForm.querySelector('input[name="Name"]');
+  const phone = currentForm.querySelector('input[type="tel"]');
+  const question = currentForm.querySelector('textarea[name="Question"]');
+  // const mail = currentForm.querySelector('input[type="email"]');
+  const check = currentForm.querySelector('input[type="checkbox"]');
+  const nameValue = name.value.trim();
+  const phoneValue = phone.value.trim();
+  const questValue = question.value.trim();
+  // const mailValue = mail.value.trim();
+  let phoneValueSplited;
+  let isNameValid = false;
+  let isPhoneValid = false;
+  let isQuestValid = false;
+  // let isMailValid = true;
+  let isChecked = false;
+
+  if (phoneValue) {
+    phoneValueSplited = phoneValue.split('+')[1].split(' ').join('').trim();
+  }
+
+  if (nameValue !== '') {
+    isNameValid = true;
+    setSuccessFor(name);
+  } else {
+    setErrorFor(name, 'Данные не верны');
+  }
+
+  if (phoneValue && phoneValueSplited.length !== 11) {
+    setErrorFor(phone, 'Данные не верны');
+  } else if (phoneValue === '') {
+    setErrorFor(phone, 'Данные не верны');
+  } else {
+    isPhoneValid = true;
+    setSuccessFor(phone);
+  }
+
+  // if (!mailValue.includes('@') || mailValue === '') {
+  //   isMailValid = false;
+  //   setErrorFor(mail, 'Данные не верны');
+  // } else {
+  //   setSuccessFor(mail);
+  // }
+  //
+
+  if (!questValue === '') {
+    isQuestValid = true;
+    setSuccessFor(question);
+  } else {
+    setErrorFor(question, 'Данные не верны');
+  }
+
+  if (check.checked) {
+    isChecked = true;
+    setSuccessFor(check);
+  } else {
+    setErrorFor(check, 'У вас нет выбора)');
+  }
+
+  return isPhoneValid && isQuestValid && isNameValid && isChecked;
+}
 
 
-// function setSuccessFor (input) {
-//   const inputControl = input.parentElement;
-//   inputControl.classList.remove('form__input-control--error');
-// }
+function setErrorFor (input, message) {
+  const inputControl = input.parentElement;
+  const small = inputControl.querySelector('small');
+  inputControl.classList.add('form__input-control--error');
+  small.textContent = message;
+}
+
+
+function setSuccessFor (input) {
+  const inputControl = input.parentElement;
+  inputControl.classList.remove('form__input-control--error');
+}
 })();
